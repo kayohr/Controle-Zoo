11b4d096d6c1e0d02064557e79534f93b6ddcb65
kayo <kayohramos@gmail.com>
2022-08-29 19:03:57 -0300

    exercicios feitos 2,3,6,7

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,8 +2,8 @@ const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const media = species.filter((element) => element.residents <= age);
-  return media;
+  return species.find((element) => element.name === animal)
+    .residents.every((resident) => resident.age >= age);
 }
-console.log((getAnimalsOlderThan('penguins', 7)));
+
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,23 +2,14 @@ const data = require('../data/zoo_data');
 const { species, employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  const referencia = employees.filter((element) => element);
-  if (typeof employeeName !== 'string') {
-    return {};
+  const res = employees.find((element) => element.firstName === employeeName || element.lastName === employeeName);
+  if (res) {
+    return res;
   }
-  if (employeeName === 'Nigel') {
-    return referencia;
-  }
-  if (employeeName === 'Nelson') {
-    return referencia;
-  }
+  return {};
 }
 // employees dados
 
 console.log(getEmployeeByName('Nigel'));
 
 module.exports = getEmployeeByName;
-
-/* if (employeeName === 'Nigel' || employeeName === 'Nelson') {
-  return employees;
-} */
