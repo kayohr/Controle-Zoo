52819041b8f2a68e2af3dbbcb36afa5305089281
kayo <kayohramos@gmail.com>
2022-08-29 21:40:11 -0300

    1,2,3-6,7

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,19 +1,9 @@
 const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  /*if (ids !== '') {
-    return [];
-  }*/
-  const vazio = [];
-  const especie = species.filter((element) => element.id === ids);
-  return especie;
-
-  /* const animals = [species];
-
-  const animalId = animals.map((elemento) => elemento.ID);
-  return animalId; */
+function getSpeciesByIds(...ids) {
+  return species.filter((animal) => ids.includes(animal.id));
 }
-// console.log(animalId);
-console.log(getSpeciesByIds());
+
+console.log(getSpeciesByIds('ef3778eb-2844-4c7c-b66c-f432073e1c6b'));
 module.exports = getSpeciesByIds;
