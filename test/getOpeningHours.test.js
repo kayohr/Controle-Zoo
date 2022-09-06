fc6a4e5028757d27d1629c1221b49391d0a9ef6f
kayo <kayohramos@gmail.com>
2022-09-06 12:17:52 -0300

    1-8,12-13,

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -12,3 +12,15 @@ it('Neste dia e hora, o zoo está aberto.', () => {
 it('Neste dia e hora, o zoo se encontra fechado.', () => {
   expect(getOpeningHours('Wednesday', '09:00-AM')).not.toBe('The zoo is closed');
 });
+it('O dia para funcionar tem que ser valido, como na segunda feira', () => {
+  expect(() => getOpeningHours('Thu', '09:00-AM')).toThrowError('The day must be valid. Example: Monday');
+});
+it('Deve ser AM ou PM', () => {
+  expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrowError('The abbreviation must be \'AM\' or \'PM\'');
+});
+it('A hora deve representar um número', () => {
+  expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrowError('The hour should represent a number');
+});
+it('As atas devem representar um número', () => {
+  expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrowError('The minutes should represent a number');
+});
