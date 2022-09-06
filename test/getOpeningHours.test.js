ba4792e12c8bf9e30a196ec8a880df26ac30dada
kayo <kayohramos@gmail.com>
2022-09-06 11:47:21 -0300

    1-8,12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,14 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Neste hórario o zoo está fechado.', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+});
+it('Neste dia e hora, o zoo está aberto.', () => {
+  expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+});
+
+it('Neste dia e hora, o zoo se encontra fechado.', () => {
+  expect(getOpeningHours('Wednesday', '09:00-AM')).not.toBe('The zoo is closed');
+});
