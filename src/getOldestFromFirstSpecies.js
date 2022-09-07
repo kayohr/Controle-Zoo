4ddfb791d9baf5aef66958f9111e80bdef7c1531
kayo <kayohramos@gmail.com>
2022-09-07 10:43:21 -0300

    1-8,10,12-13

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,10 +3,18 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   const colaboradores = employees.find((colab) => colab.id === id).responsibleFor[0];
-  console.log(colaboradores);
+  // console.log(colaboradores);
   const animal = species.find((animals) => animals.id === colaboradores).residents;
-  return animal;
+  const newAnimal = animal.reduce((a, b) => ((a.age > b.age) ? a : b));
+  return Object.values(newAnimal);
+
+  // animal.map((g) => g.age.employees[0]).sort((a, b) => b - a);
 }
 
-console.log(getOldestFromFirstSpecies('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
+
+// var elMaximo = [4,3,6,9].sort(function(a, b){return b - a;})[0];
+/* const higherAge = users.reduce(function(prev, current) {
+  return (prev.age > current.age) ? prev : current
+}) */
